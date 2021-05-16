31be6cf73d132a189acb4245069b5e9f8a1edcb8
lgypro <kaixin.lgx@gmail.com>
2021-05-16 22:48:30 +0800

    add operation about access key

--- iam/create_iam_user.sh
+++ iam/create_iam_user.sh
@@ -40,10 +40,34 @@ aws iam create-login-profile --generate-cli-skeleton  > create-login-profile.jso
 # 应用模板文件
 aws iam create-login-profile --cli-input-json file://create-login-profile.json
 
-# 为IAM用户生成访问密钥，用于编程访问
+# 为IAM用户生成访问密钥，用于编程访问（最多创建两个，Cannot exceed quota for AccessKeysPerUser: 2）
 aws iam create-access-key --user-name jerry
 
 # 将用户添加到用户组
 aws iam add-user-to-group --generate-cli-skeleton > user-goup.json
 # 修改模板文件内容
 aws iam add-user-to-group --cli-input-json file:///root/user-goup.json
+
+# 直接为用户关联策略
+aws iam attach-user-policy --user-name user-can-create-other-user --policy-arn arn:aws:iam::345164961032:policy/OnlyCanCreateUser
+
+# 我是谁？（这个操作很特殊，不需要权限，在策略中被显式拒绝了也能执行成功）
+aws sts get-caller-identity
+
+# 删除IAM用户
+aws iam delete-user --user-name wuyingge
+
+# 查看所有IAM用户
+aws --no-paginate iam list-users
+
+# 查看IAM用户名下的access key
+aws iam list-access-keys --user-name user-can-create-other-user
+
+# 更新access key的状态（启用或者禁用）
+aws iam update-access-key --user-name user-can-create-other-user --access-key-id AKIAVAXLPKUEKAG6NYWR  --status Inactive
+
+# 删除某一个access key
+aws iam delete-access-key --user-name user-can-create-other-user --access-key-id AKIAVAXLPKUENLN2GGFC
+
+# 修改自己的密码（只能是IAM用户）
+aws iam change-password --old-password 'wGysGSLnek3sYny' --new-password 'YDZwF&E_8gfqht2w+'
